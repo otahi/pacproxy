56178cc3b8a318f509cd1c49837716fc9dc172c9
OTA Hiroshi <ota_h@nifty.com>
2014-07-27 07:33:03 +0900

    Fix rubocop indications for spec

--- spec/pacproxy_spec.rb
+++ spec/pacproxy_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 require 'httpclient'
 
 def wait_server_status(servers, status)
-  return unless (servers || status)
+  return unless servers || status
   servers = [servers] unless servers.respond_to?(:all?)
   return unless servers.all? { |s| s.respond_to?(:status) }
-  sleep(0.01) until servers.all? {|s| s.status == status }
+  sleep(0.01) until servers.all? { |s| s.status == status }
 end
 
 describe Pacproxy do
@@ -28,27 +28,29 @@ describe Pacproxy do
   describe 'Pacproxy#proxy_uri' do
     before(:each) do
       $stdout, $stderr = StringIO.new, StringIO.new
-      $http_server = WEBrick::HTTPServer.new(Port: 13080)
-      $http_server.mount_proc('/') { |req, res| res.status = 200}
-      $http_server.mount_proc('/noproxy/') { |req, res| res.status = 200}
-      $proxy_server = WEBrick::HTTPProxyServer.new(Port: 13081)
-      Thread.new { $http_server.start }
-      Thread.new { $proxy_server.start }
-      wait_server_status([$http_server,$proxy_server], :Running)
+      @http_server = WEBrick::HTTPServer.new(Port: 13_080)
+      @http_server.mount_proc('/') { |_req, res| res.status = 200 }
+      @http_server.mount_proc('/noproxy/') { |_req, res| res.status = 200 }
+      @proxy_server = WEBrick::HTTPProxyServer.new(Port: 13_081)
+      Thread.new { @http_server.start }
+      Thread.new { @proxy_server.start }
+      wait_server_status([@http_server, @proxy_server], :Running)
     end
 
     after(:each) do
       $stdout, $stderr = STDOUT, STDERR
-      $http_server.shutdown
-      $proxy_server.shutdown
-      $pacproxy_server.shutdown
-      wait_server_status([$http_server,$proxy_server,$pacproxy_server], :Stop)
+      @http_server.shutdown
+      @proxy_server.shutdown
+      @pacproxy_server.shutdown
+      wait_server_status([@http_server, @proxy_server, @pacproxy_server], :Stop)
     end
 
     it 'transfer request to server directly' do
-      $pacproxy_server = Pacproxy::Pacproxy.new(Port: 13128, Proxypac: 'spec/all_direct.pac')
-      Thread.new { $pacproxy_server.start }
-      wait_server_status($pacproxy_server, :Running)
+      @pacproxy_server =
+        Pacproxy::Pacproxy.new(Port: 13_128,
+                               Proxypac: 'spec/all_direct.pac')
+      Thread.new { @pacproxy_server.start }
+      wait_server_status(@pacproxy_server, :Running)
 
       c = HTTPClient.new('http://127.0.0.1:13128')
       res = c.get('http://127.0.0.1:13080/')
@@ -59,9 +61,11 @@ describe Pacproxy do
     end
 
     it 'transfer request to server via parent proxy' do
-      $pacproxy_server = Pacproxy::Pacproxy.new(Port: 13128, Proxypac: 'spec/all_proxy.pac')
-      Thread.new { $pacproxy_server.start }
-      wait_server_status($pacproxy_server, :Running)
+      @pacproxy_server =
+        Pacproxy::Pacproxy.new(Port: 13_128,
+                               Proxypac: 'spec/all_proxy.pac')
+      Thread.new { @pacproxy_server.start }
+      wait_server_status(@pacproxy_server, :Running)
 
       c = HTTPClient.new('http://127.0.0.1:13128')
       res = c.get('http://127.0.0.1:13080/')
@@ -72,9 +76,11 @@ describe Pacproxy do
     end
 
     it 'transfer request to server via parent proxy partially' do
-      $pacproxy_server = Pacproxy::Pacproxy.new(Port: 13128, Proxypac: 'spec/partial_proxy.pac')
-      Thread.new { $pacproxy_server.start }
-      wait_server_status($pacproxy_server, :Running)
+      @pacproxy_server =
+        Pacproxy::Pacproxy.new(Port: 13_128,
+                               Proxypac: 'spec/partial_proxy.pac')
+      Thread.new { @pacproxy_server.start }
+      wait_server_status(@pacproxy_server, :Running)
 
       c = HTTPClient.new('http://127.0.0.1:13128')
       res = c.get('http://127.0.0.1:13080/')
