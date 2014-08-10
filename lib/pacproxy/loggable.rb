9aaf36dc4e530d7c6d6a696dc99d12b32cd76378
OTA Hiroshi <ota_h@nifty.com>
2014-08-11 06:16:13 +0900

    Delete unnecessary methods

--- lib/pacproxy/loggable.rb
+++ lib/pacproxy/loggable.rb
@@ -16,40 +16,20 @@ module Pacproxy
       general_logger.debug(message)
     end
 
-    def debug?
-      general_logger.debug?
-    end
-
     def info(message)
       general_logger.info(message)
-    end
-
-    def info?
-      general_logger.info?
     end
 
     def lwarn(message)
       general_logger.warn(message)
     end
 
-    def warn?
-      general_logger.warn?
-    end
-
     def error(message)
       general_logger.error(message)
     end
 
-    def error?
-      general_logger.error?
-    end
-
     def fatal(message)
       general_logger.fatal(message)
-    end
-
-    def fatal?
-      general_logger.fatal?
     end
 
     def accesslog(req, res)
