0d16d42c1662cddabdb7f3021cfae93290b77ec8
OTA Hiroshi <ota_h@nifty.com>
2014-09-26 13:44:08 +0900

    Change calling Node.js function via socket for speed

--- lib/pacproxy/runtimes/node/node.rb
+++ lib/pacproxy/runtimes/node/node.rb
@@ -1,5 +1,8 @@
 require 'pacproxy'
+require 'tempfile'
 require 'open-uri'
+require 'dnode'
+require 'thread'
 
 module Pacproxy
   module Runtime
@@ -18,9 +21,22 @@ module Pacproxy
         new
       end
 
+      def initialize
+        @socket = File.join(Dir.tmpdir, "pacproxy-#{rand_string}")
+
+        Thread.new(@socket) do |s|
+          begin
+            js = File.join(File.dirname(__FILE__), 'find.js')
+            server = fork { exec "node #{js} #{s}" }
+          ensure
+            Process.kill(server)
+            File.delete(@socket)
+          end
+        end
+        loop until File.exist?(@socket)
+      end
+
       def load(file_location)
-        # TODO: delete file_location to replace
-        @file_location = file_location
         @source = open(file_location, proxy: false).read
         self
       rescue
@@ -31,8 +47,26 @@ module Pacproxy
       def find(url)
         return 'DIRECT' unless @source
         uri = URI.parse(url)
-        js = File.join(File.dirname(__FILE__), 'FindProxyUrl.js')
-        `node #{js} "#{@file_location}" #{uri} #{uri.host}`.chomp
+
+        call_find(uri)
+      end
+
+      private
+
+      def call_find(uri)
+        proxy = nil
+        DNode.new.connect(@socket) do |remote|
+          remote.find(@source, uri, uri.host,
+                      proc do |p|
+                        proxy = p
+                        EM.stop
+                      end)
+        end
+        proxy
+      end
+
+      def rand_string
+        (0...16).map { ('a'..'z').to_a[rand(26)] }.join
       end
     end
   end
