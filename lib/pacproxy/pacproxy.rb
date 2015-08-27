9ea84e9845b00b5e21961457fa243cb67024c69e
Hiroshi Ota <otahi.pub@gmail.com>
2015-08-27 09:39:09 +0900

    Temp add a hook for method call

--- lib/pacproxy/pacproxy.rb
+++ lib/pacproxy/pacproxy.rb
@@ -2,6 +2,10 @@ require 'pacproxy'
 require 'socket'
 require 'uri'
 
+set_trace_func proc { |event, _file, _line, id, _binding, classname|
+  puts "#{classname} #{id} called" if event == 'call' && !(classname.to_s.include?('SimpleCov'))
+}
+
 module Pacproxy
   # Pacproxy::Pacproxy represent http/https proxy server
   class Pacproxy
